2239622c9b6415cb1279fbcff58cc44857b89358
zocraOS <pascaljbarth@web.de>
2019-12-13 18:27:52 +0100

    Initial Commit

---
+++ Buro_Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Buro_Game
+//
+//  Created by Pascal Barth on 13.12.19.
+//  Copyright © 2019 zocraOS. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
